ad1d57f4f95e4d870af653e293a0babaf0bfd8b7
NB670 <neel468167@gmail.com>
2026-06-01 10:56:03 -0700

    fix: remove old voice:state listener before registering new one

--- electron/preload.js
+++ electron/preload.js
@@ -1,9 +1,12 @@
 'use strict'
 const { contextBridge, ipcRenderer } = require('electron')
 contextBridge.exposeInMainWorld('isElectron', true)
+let _stateListener = null
 contextBridge.exposeInMainWorld('voiceAPI', {
   onStateChange: (cb) => {
-    ipcRenderer.on('voice:state', (_event, data) => cb(data))
+    if (_stateListener) ipcRenderer.removeListener('voice:state', _stateListener)
+    _stateListener = (_event, data) => cb(data)
+    ipcRenderer.on('voice:state', _stateListener)
   },
   close: () => {
     ipcRenderer.send('voice:close')
